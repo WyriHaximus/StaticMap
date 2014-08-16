0cf5b73cc4931aec1220e8c7886dd35546a24563
Cees-Jan <ceesjank@gmail.com>
2014-08-16 17:25:03 +0200

    Always return a value

--- src/LatLng.php
+++ src/LatLng.php
@@ -52,7 +52,7 @@ final class LatLng
      */
     public function setLat($lat)
     {
-        $this->lat = $this->sanitized($lat, -90, 90);
+        $this->lat = $this->sanitized($lat, -90, 90, $this->lat);
 
         return $this->lat;
     }
@@ -73,7 +73,7 @@ final class LatLng
      */
     public function setLng($lng)
     {
-        $this->lng = $this->sanitized($lng, -180, 180);
+        $this->lng = $this->sanitized($lng, -180, 180, $this->lng);
 
         return $this->lng;
     }
@@ -93,16 +93,17 @@ final class LatLng
      * @param  int $value      Value to be sanitized
      * @param  int $rangeBegin Begin of the range
      * @param  int $rangeEnd   End of the range
-     * @return int|bool
+     * @param  int $default    Value to use incase $value is invalid
+     * @return int
      */
-    private function sanitized($value, $rangeBegin, $rangeEnd)
+    private function sanitized($value, $rangeBegin, $rangeEnd, $default)
     {
         $value = floatval($value);
 
         if ($value >= $rangeBegin && $rangeEnd >= $value) {
             return $value;
         } else {
-            return false;
+            return $default;
         }
     }
 
